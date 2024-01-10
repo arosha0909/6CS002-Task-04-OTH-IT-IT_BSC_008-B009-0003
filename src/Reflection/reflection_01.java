62f80cafd267d2856b3d76b4602cf210482d3560
Arosha Rukshan <rukshanarosha@gmail.com>
2024-01-10 15:19:03 +0530

    reflection 01

---
+++ src/Reflection/reflection_01.java
@@ -0,0 +1,8 @@
+package Reflection;
+
+public class reflection_01 {
+    public static void main(String[] args) {
+        Simple p = new Simple();
+        System.out.println("p = " + p);
+    }
+}
